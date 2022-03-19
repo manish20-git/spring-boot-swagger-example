be1c15d537788fa06766a20ac8e0f63b162671ed
manish mansare <mansaremanish10@gmail.com>
2022-03-20 01:14:19 +0530

    added WelcomeController

---
+++ src/main/java/com/techprimers/springboot/swaggerexample/resource/WelcomeController.java
@@ -0,0 +1,15 @@
+package com.techprimers.springboot.swaggerexample.resource;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping( "/welcome")
+public class WelcomeController {
+
+    public String welcome()
+    {
+        return "Welcome Manish";
+    }
+
+}
